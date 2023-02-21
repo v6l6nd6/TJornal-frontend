37ce24eca53aa4a0dec010539fde7efac443e844
Voland <marinyu32@gmail.com>
2023-02-20 19:28:34 -0800

    first-commit

---
+++ components/FullPost/index.tsx
@@ -0,0 +1,53 @@
+import { Button, Paper, Typography } from '@material-ui/core';
+import React from 'react';
+import { PostActions } from '../PostActions';
+import MessageIcon from '@material-ui/icons/TextsmsOutlined';
+import UserAddIcon from '@material-ui/icons/PersonAddOutlined';
+
+import styles from './FullPost.module.scss';
+import { OutputData } from '@editorjs/editorjs';
+
+interface FullPostProps {
+  title: string;
+  blocks: OutputData['blocks'];
+}
+
+export const FullPost: React.FC<FullPostProps> = ({ title, blocks }) => {
+  return (
+    <Paper elevation={0} className={styles.paper}>
+      <div className="container">
+        <Typography variant="h4" className={styles.title}>
+          {title}
+        </Typography>
+        <div className={styles.text}>
+          {blocks.map((obj) => (
+            <Typography key={obj.id} dangerouslySetInnerHTML={{ __html: obj.data.text }} />
+          ))}
+
+          <div style={{ width: 250, marginLeft: -14 }}>
+            <PostActions />
+          </div>
+          <div className="d-flex justify-between align-center mt-30 mb-30">
+            <div className={styles.userInfo}>
+              <img
+                src="https://leonardo.osnova.io/104b03b4-5173-fd9f-2af9-b458dddc4a23/-/scale_crop/108x108/-/format/webp/"
+                alt="Avatar"
+              />
+              <b>Donnie Darko</b>
+              <span>+1685</span>
+            </div>
+            <div>
+              <Button variant="contained" className="mr-15">
+                <MessageIcon />
+              </Button>
+              <Button variant="contained">
+                <UserAddIcon />
+                <b className="ml-10">Подписаться</b>
+              </Button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Paper>
+  );
+};
---
+++ pages/index.tsx
@@ -0,0 +1,40 @@
+import { Post } from '../components/Post';
+import { MainLayout } from '../layouts/MainLayout';
+import { Api } from '../utils/api';
+import { NextPage } from 'next';
+import { PostItem } from '../utils/api/types';
+
+interface HomeProps {
+  posts: PostItem[];
+}
+
+const Home: NextPage<HomeProps> = ({ posts }) => {
+  console.log(posts);
+  return (
+    <MainLayout>
+      {posts.map((obj) => (
+        <Post key={obj.id} id={obj.id} title={obj.title} description={obj.description} />
+      ))}
+    </MainLayout>
+  );
+};
+
+export const getServerSideProps = async (ctx) => {
+  try {
+    const posts = await Api().post.getAll();
+    return {
+      props: {
+        posts,
+      },
+    };
+  } catch (err) {
+    console.log(err);
+  }
+  return {
+    props: {
+      posts: null,
+    },
+  };
+};
+
+export default Home;
